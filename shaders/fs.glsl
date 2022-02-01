65c1e00debb3bd7a9c11f583fa1f1cdbb878acc9
Roman Kotelnikov <roman.kotelnikov@outlook.com>
2022-02-01 22:51:15 +0900

    Cleanup shaders some more

--- shaders/fs.glsl
+++ shaders/fs.glsl
@@ -1,14 +1,14 @@
 in vec3 v_norm;
-in vec4 v_color;
 
 uniform mat4 normalMatrix;
 uniform vec3 lightPosition;
+uniform vec4 color;
 
 out vec4 frag_color;
 
 void main() {
   vec3 norm_transformed = (normalMatrix * vec4(v_norm, 0.)).xyz;
-  float kd = dot((norm_transformed, -normalize(lightPosition)) + .3;
+  float kd = dot(norm_transformed, -normalize(lightPosition)) + .3;
 
-  frag_color = v_color * kd;
+  frag_color = color * kd;
 }
--- shaders/vs.glsl
+++ shaders/vs.glsl
@@ -4,16 +4,12 @@ in vec3 norm;
 uniform mat4 rotation;
 uniform mat4 projection;
 uniform mat4 view;
-uniform vec4 color;
 
 out vec3 v_norm;
-out vec4 v_color;
 
 void main() {
   vec4 vertex_position = rotation * vec4(position, 1.);
-  // vec4 vertex_position = vec4(position, 1.);
 
-  v_color = color;
   v_norm = norm;
 
   gl_Position = projection * view * vertex_position;
