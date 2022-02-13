4ac717c5035a8399d846ac5cea76645909388e9b
Roman Kotelnikov <roman.kotelnikov@outlook.com>
2022-02-13 23:03:46 +0900

    New Build: add new filter type

--- docs/bootstrap.js
+++ docs/bootstrap.js
@@ -516,7 +516,7 @@
 /******/ 				promises.push(installedWasmModuleData);
 /******/ 			else {
 /******/ 				var importObject = wasmImportObjects[wasmModuleId]();
-/******/ 				var req = fetch(__webpack_require__.p + "" + {"../pkg/florest_bg.wasm":"caaf50ae64561025ff02"}[wasmModuleId] + ".module.wasm");
+/******/ 				var req = fetch(__webpack_require__.p + "" + {"../pkg/florest_bg.wasm":"fb4c2ed5d90b58baaf4d"}[wasmModuleId] + ".module.wasm");
 /******/ 				var promise;
 /******/ 				if(importObject instanceof Promise && typeof WebAssembly.compileStreaming === 'function') {
 /******/ 					promise = Promise.all([WebAssembly.compileStreaming(req), importObject]).then(function(items) {
